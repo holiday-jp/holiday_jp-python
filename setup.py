34f1da7e602b9e9ab0ff2056d071664e5c74d185
tessier <tessier@luxeys.com>
2019-04-23 10:21:24 +0900

    [release]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='holiday_jp',
-      version='19.02.20',
+      version='19.04.23',
       url='https://github.com/LUXEYS/holiday_jp-python',
       license='MIT',
       author='Luxeys',
