1f117d99e02eb3f4b286a7a49f4136ced2ae5e38
gabriel-tessier <gabriel-tessier@users.noreply.github.com>
2019-09-24 23:38:40 +0900

    Update setup.py
    Add classifiers, reflects changes in travis
    + install requires.

--- setup.py
+++ setup.py
@@ -10,4 +10,19 @@ setup(name='holiday_jp',
       long_description_content_type="text/markdown",
       packages=find_packages(),
       long_description=open('README.md').read(),
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=[
+        "python-dateutil==2.7.5",
+      ],
+      classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: MIT",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8-dev",
+        "Programming Language :: Python :: nightly",
+])
